771cbd06f45b018ee5d5cd12827405a739b7dbc4
Ch. König <criss@paars.de>
2018-09-08 20:49:12 +0200

    fixed missing promise (use bluebird instead of promisfy-node)

--- plugins/index.js
+++ plugins/index.js
@@ -1,23 +1,22 @@
 const logger = require('../app/logger')
-var fs = require('fs')
-var path = require('path')
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs'))
+const path = require('path')
 
-exports.init = function init(app) {
-  return async function init() {
-    var files = await fs.readdir(__dirname)
+exports.init = async function init(app) {
+  let files = await fs.readdirAsync(__dirname)
 
-    files
-      .filter(file => file.indexOf('.') !== 0 && file !== 'index.js')
-      .forEach(async file => {
-        const filePath = path.join(__dirname, file)
-        logger.debug('loading plugin from: %s', filePath)
+  files
+    .filter(file => file.indexOf('.') !== 0 && file !== 'index.js')
+    .forEach(async file => {
+      const filePath = path.join(__dirname, file)
+      logger.debug('loading plugin from: %s', filePath)
 
-        try {
-          const plugin = require(filePath)
-          await plugin.init(app)
-        } catch (err) {
-          logger.error('failed to load plugin: ', err)
-        }
-      })
-  }
+      try {
+        const plugin = require(filePath)
+        await plugin.init(app)
+      } catch (err) {
+        logger.error('failed to load plugin: ', err)
+      }
+    })
 }
