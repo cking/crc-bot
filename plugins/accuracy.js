9848da63d062415b096d16c741e676fa2483a396
Ch. König <criss@paars.de>
2018-09-08 20:51:43 +0200

    redo accuracy command

--- plugins/accuracy.js
+++ plugins/accuracy.js
@@ -1,30 +1,20 @@
-'use strict';
-var logger = require('winston');
-var co = require('co');
-
-var app;
-
-function init(a) {
-    app = a;
-    app.addCommand({
-        name: "acc",
-        desc: "Display current Accuracy Caps for Raids in FF14",
-        exec: acc
-    });
-    return Promise.resolve();
+exports.init = function init(app) {
+  app.addCommand({
+    name: 'acc',
+    desc: 'Display current Accuracy Caps for Raids in FF14',
+    exec: acc
+  })
 }
 
 function acc(cmd) {
-    return co(function *init() {
-
-        return cmd.dest.send("```" + [
-            "                Front   Flank   Caster",
-            "Alex Savage	 699     646     592",
-            "Zurvan  	    699     646     582",
-            "Stormblood     NONE LOL"
-        ].join("\n") + "```");
-
-    });
+  return cmd.dest.send(
+    '```' +
+      [
+        '                Front   Flank   Caster',
+        'Alex Savage     699     646     592',
+        'Zurvan          699     646     582',
+        'Stormblood      NONE LOL'
+      ].join('\n') +
+      '```'
+  )
 }
-
-module.exports.init = init;
