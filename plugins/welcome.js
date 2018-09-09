dae128563eb6d06a1a9e8124f3abafaa9854a180
Ch. König <criss@paars.de>
2018-09-09 08:41:28 +0200

    updated welcome message

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -1,30 +1,20 @@
-'use strict';
-var logger = require('winston');
-var co = require('co');
+const config = require('../app/config')
+const logger = require('../app/logger')
 
-var config;
-
-function init(app) {
-    config = app.config.welcome;
-    app.client.on('guildMemberAdd', guildMemberAdd);
-    app.client.on('guildMemberRemove', guildMemberRemove);
-    return Promise.resolve();
+exports.init = function init(app) {
+  app.client.on('guildMemberAdd', guildMemberAdd)
+  app.client.on('guildMemberRemove', guildMemberRemove)
+  return Promise.resolve()
 }
 
 function guildMemberAdd(member) {
-    return co(function *guildMemberAdd() {
-
-        logger.verbose("%s (%s) joined the CRC", member.displayName, member.user);
-        var msg = config.message.
-            replace(/:NAME:/g, member.displayName).
-            replace(/:MENTION:/g, member.user);
-        return member.send(msg);
-
-    });
+  logger.verbose('%s (%s) joined the CRC', member.displayName, member.user)
+  const msg = config.message
+    .replace(/:NAME:/g, member.displayName)
+    .replace(/:MENTION:/g, member.user)
+  return member.send(msg)
 }
 
 function guildMemberRemove(member) {
-    logger.info("%s (%s) left the CRC", member.displayName, member.user);
+  logger.info('%s (%s) left the CRC', member.displayName, member.user)
 }
-
-module.exports.init = init;
